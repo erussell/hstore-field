7675fafd42aaefcb9c582ddb708885acb56940d3
Eric Russell <erussell@ngs.org>
2012-11-05 16:56:07 -0500

    Add manifest file so the readme & license get included in package, and bump the version number for pkgbin

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='hstore-field',
-    version='1.0.0',
+    version='1.0.1',
     description="Support for PostgreSQL's hstore for Django.",
     long_description=open('README.md').read(),
     author='Eric Russell',
     author_email='eric-r@pobox.com',
-    license='BSD',
     url='http://github.com/erussell/hstore-field',
-    packages=[
-        'hstore_field'
-    ],
+    packages=['hstore_field'],
+    include_package_data=True,
+    license='BSD',
 )
